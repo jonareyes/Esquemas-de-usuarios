3433e3e239a5042bcd0e8d09ab3678a9e2bbd306
Jonathan <jonathan_reyesv@hotmail.com>
2016-04-05 08:56:33 -0500

    Ejercicio terminado para realizar validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,25 @@
 class User < ActiveRecord::Base
-# Implementa los métodos y validaciones de tu modelo aquí. 
+# Implementa los métodos y validaciones de tu modelo aquí.   
   def name
     "#{first_name} #{last_name}" 
   end
 
   def age 
-     Date.today.year - birthday.day - birthday.year 
-    
+     Date.today.year - birthday.day - birthday.year   
   end
+  
+   validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create}
+   validates :email, uniqueness: true
+   validates_numericality_of :age, :only_integer => true, :greater_than_or_equal_to => 18
+   # validates_format_of :phone,
+   #            :with => /\A[0-9]{10}\Z/,
+   #            :allow_blank => true,
+   #            :allow_nil => true
+
+  def before_validation_on_create
+     self.phone = phone.gsub(/[^0-9]/, "")
+  end   
+
+  validates_length_of :phone, :minimum => 10
 end
+
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,6 +1,6 @@
 require 'date'
 require 'rspec'
-  require_relative '../config/application'
+require_relative '../config/application'
 
 
 describe User, "#name and #age" do
@@ -103,7 +103,7 @@ describe User, "advanced validations" do
       :birthday => Date.new(1990,5,2),
       :gender => 'male',
       :email => 'fer@codea.mx',
-      :phone => '(456) 435-3456 x4657'
+      :phone => '(456) 435-3456' #x4657'
     )
   end
 
